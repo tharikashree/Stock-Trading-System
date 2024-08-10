fdffe31f490101cd7fb7d73f14860d5a8a84cd03
Tharika Shree R <151807542+tharikashree@users.noreply.github.com>
2024-08-10 15:41:49 +0530

    Update settings.py

--- backend/trading_platform/settings.py
+++ backend/trading_platform/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.postgresql',
          'NAME': "postgres",
          "USER": "postgres",
-         "PASSWORD": "Thari0901!",
+         "PASSWORD": "**********",
          "HOST": "localhost",
          "PORT": '5432'
      }
